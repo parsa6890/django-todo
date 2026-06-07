75671350a80690ec762b0c4933f797545c5e8a47
parsa6890 <user.parsa@gmail.com>
2026-06-07 14:35:40 +0330

    refactor todo view for better data organization

--- todo/views.py
+++ todo/views.py
@@ -4,11 +4,14 @@ from .models import TodoList
 
 
 def todo_view(request):
-	todo_list = TodoList.objects.all()
-	pending_list= todo_list.filter(status='pending')
-	done_list= todo_list.filter(status='done')
+	all_list = TodoList.objects.all()
+	pending_list= TodoList.objects.filter(status='pending')
+	inprogress_list= TodoList.objects.filter(status='in_progress')
+	done_list= TodoList.objects.filter(status='done')
 	context={
+		'all_list': all_list,
 		'pending_list': pending_list,
+		'inprogress_list': inprogress_list,
 		'done_list': done_list,
 	}
 	return render(request, 'todo/index.html', context)
