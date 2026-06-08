eefd1cc544a2d8f5996d6a38248955e242f058b2
parsa6890 <user.parsa@gmail.com>
2026-06-08 13:44:16 +0330

    feat: add task creation flow (view, template, url routing)

--- todo/urls.py
+++ todo/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
 	path('', views.todo_view, name='todos'),
+	path('creat_task/', views.cform_view, name='creat_task'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,25 +1,32 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
 from .models import TodoList
+from .forms import TodoForm
 
 
 STATUS_CHOICES = ['pending', 'in_progress', 'done']
-
 def todo_view(request):
 	all_list = TodoList.objects.all()
-	# pending_list= TodoList.objects.filter(status='pending')
-	# inprogress_list= TodoList.objects.filter(status='in_progress')
-	# done_list= TodoList.objects.filter(status='done')
 	grouped_todos = {
 		status : all_list.filter(status=status)
 				for status in STATUS_CHOICES
 	}
 	context={
-		# 'all_list': all_list,
-		# 'pending_list': pending_list,
-		# 'inprogress_list': inprogress_list,
-		# 'done_list': done_list,
 		'grouped_todos': grouped_todos
 	}
 	return render(request, 'todo/index.html', context)
+
+
+def cform_view(request):
+	if request.method == "POST":
+		form = TodoForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('todos')
+	else:
+		form= TodoForm()
+	context= {
+		"form" : form
+	}
+	return render(request, 'todo/creat_task.html', context)
 
