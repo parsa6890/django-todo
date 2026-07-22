f983a16f20585af0fdc2e59ab7012bc5bfad4425
parsa6890 <user.parsa@gmail.com>
2026-07-22 17:15:22 +0330

    Implement chat conversation and message models

--- todo/admin.py
+++ todo/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import TodoList
+from .models import TodoList, Conversation, Message
 
 
 admin.site.register(TodoList)
+admin.site.register(Conversation)
+admin.site.register(Message)
--- todo/models.py
+++ todo/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 
@@ -48,3 +49,47 @@ class UserProfile(models.Model):
 	mobile = models.CharField(max_length=12, blank=True)
 	internal_number = models.CharField(max_length=5, blank=True)
 	bio = models.TextField(max_length=255, blank=True)
+
+
+class Conversation(models.Model):
+	participants=models.ManyToManyField(User, related_name="conversations")
+	created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True,
+								related_name="created_conversations")
+	name=models.CharField(max_length=100, blank=True)
+	is_group=models.BooleanField(default=False)
+	created_at=models.DateTimeField(auto_now_add=True)
+	updated_at=models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		if self.is_group and self.name:
+			return self.name
+		return f"conversation #{self.pk}"
+	
+	def clean(self):
+		super().clean()
+
+		if not self.is_group and self.pk:
+			participants = self.participants.all()
+
+			if participants.count() != 2:
+				raise ValidationError(
+					"A private conversation must have exactly two participants."
+				)
+
+		def save(self, *args, **kwargs):
+			self.full_clean()
+			super().save(*args, **kwargs)
+
+
+class Message(models.Model):
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name="messages")
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="messages")
+    text = models.TextField()
+    reply_to = models.ForeignKey("self", on_delete=models.SET_NULL, null=True, blank=True, 
+								 related_name="replies")
+    created_at = models.DateTimeField(auto_now_add=True)
+    edited_at = models.DateTimeField(null=True,blank=True,)
+
+	
+    def __str__(self):
+        return self.sender.username
